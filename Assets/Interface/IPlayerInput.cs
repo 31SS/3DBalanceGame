7054f1c978cb2dfd9d426301cc437e6e388f414f
DESKTOP-89F0FVT\SS <sato991105@outlook.jp>
2020-11-21 23:08:48 +0900

    初コミット

---
+++ Assets/Interface/IPlayerInput.cs
@@ -0,0 +1,16 @@
+﻿//キーボード等の操作機器を容易に切り替えられるように用意したインターフェース
+//現在はキーボードのみ対応
+public interface IPlayerInput
+{
+    void Inputting();
+
+    float X
+    {
+        get; set;
+    }
+
+    bool Jump
+    {
+        get; set;
+    }
+}
---
+++ Assets/Script/CharacterControllerScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterControllerScript : MonoBehaviour
+{
+
+    public float speed = 3.0F;
+    public float rotateSpeed = 3.0F;
+
+    private CharacterController controller;
+
+    void Start()
+    {
+
+        // コンポーネントの取得
+        controller = GetComponent<CharacterController>();
+
+    }
+
+    void Update()
+    {
+
+        // 回転
+        transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+
+        // キャラクターのローカル空間での方向
+        Vector3 forward = transform.transform.forward;
+
+        float curSpeed = speed * Input.GetAxis("Vertical");
+
+        // SimpleMove関数で移動させる
+        controller.SimpleMove(forward * curSpeed);
+
+    }
+
+}
