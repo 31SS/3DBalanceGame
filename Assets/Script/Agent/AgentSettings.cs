0a4559294e9bbf296bfeedd1d0269d6730ed5b0a
DESKTOP-89F0FVT\SS <sato991105@outlook.jp>
2020-12-15 18:53:52 +0900

    Enemy 強化学習 1回目

--- Assets/Script/Agent/AgentSettings.cs
+++ Assets/Script/Agent/AgentSettings.cs
@@ -6,7 +6,7 @@ public class AgentSettings : MonoBehaviour
 {
     [Header("Specific to 3DBalance")]
     public float agentRunSpeed;
-    public float agentJumpHeight;
+    public float agentJumpPower;
     //when a goal is scored the ground will use this material for a few seconds.
     public Material goalScoredMaterial;
     //when fail, the ground will use this material for a few seconds.
--- Assets/Script/Enemy/EnemyAgent.cs
+++ Assets/Script/Enemy/EnemyAgent.cs
@@ -1,15 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 using Unity.Mathematics;
 using Unity.MLAgents;
 using CharacterState;
 using UniRx;
+using Unity.Barracuda;
+using Unity.MLAgents.Actuators;
 using Unity.MLAgents.Sensors;
-
+using Random = UnityEngine.Random;
 
 public class EnemyAgent : Agent
 {
-     //変更前のステート名
+    //変更前のステート名
     protected string _prevStateName;
     
     //ステート
@@ -21,17 +24,19 @@ public class EnemyAgent : Agent
     public  CharacterStateClear StateClear { get; set; } = new CharacterStateClear();
     public CharacterStateDie StateDie { get; set; } = new CharacterStateDie();
     
-    Rigidbody m_Agentrb;
     public Transform target;
-    private AgentSettings m_AgentSettings;
-    
+    public GameObject start;
+    public GameObject[] goalRespawnPos;
+    private Rigidbody m_AgentRb;
+    private Rigidbody m_StartRb;
+    private Rigidbody m_TargetRb;
+
     [SerializeField] protected Animator _animator;
     
     [SerializeField] protected bool isGround;
-
-    protected PlayerInput _playerInput;
-    protected RaycastHit _isGroundHit;
-    protected Vector3 latestPos;
+    
+    private RaycastHit _isGroundHit;
+    private Vector3 latestPos;
     private Ray isGroundRay;
     [SerializeField] protected Vector3 target_dir;
     [SerializeField] protected float magnitude;
@@ -40,7 +45,6 @@ public class EnemyAgent : Agent
     [SerializeField] protected float RUN_SPEED;
     [SerializeField] protected float JUMP_POWER;
     [SerializeField] protected float SMOOTH;
-    
     
     protected void Awake()
     {
@@ -52,15 +56,13 @@ public class EnemyAgent : Agent
         StateClear.ExecAction = Clear;
         StateDie.ExecAction = Die;
         isGround = false;
-        
-        _playerInput = new PlayerInput();
     }
 
     public override void Initialize()
     {
-        m_AgentSettings = FindObjectOfType<AgentSettings>();
-        
-        m_Agentrb.GetComponent<Rigidbody>();
+        m_AgentRb = GetComponent<Rigidbody>();
+        m_StartRb = start.GetComponent<Rigidbody>();
+        m_TargetRb = target.GetComponent<Rigidbody>();
     }
     
 
@@ -78,48 +80,112 @@ public class EnemyAgent : Agent
             .AddTo(this);
     }
 
+    public override void OnEpisodeBegin()
+    {
+        ResetGoal();
+        if (transform.localPosition.y < -1f)
+        {
+            transform.localPosition = m_StartRb.transform.localPosition;
+            transform.localRotation = Quaternion.Euler(0,180,0);
+            m_AgentRb.velocity = Vector3.zero;
+        }
+    }
+
     public override void CollectObservations(VectorSensor sensor)
     {
         sensor.AddObservation(target.transform.position);
         sensor.AddObservation(transform.localPosition);
         sensor.AddObservation(transform.rotation);
-        
+        sensor.AddObservation(Physics.Raycast(isGroundRay, out _isGroundHit));
         sensor.AddObservation(GroundCheck(isGroundRay) ? 1 : 0);
     }
-
-    protected void Update()
+    
+    // public override void OnActionReceived(ActionBuffers actions)
+    // {
+    //     Move(actions.DiscreteActions, transform);
+    //     
+    //     if ((!Physics.Raycast(m_AgentRb.position, Vector3.down, 20)))
+    //     {
+    //         SetReward(-1f);
+    //         EndEpisode();
+    //     }
+    // }
+    
+    public override void OnActionReceived(ActionBuffers actions)
     {
-        _playerInput.Inputting();
+        
+        Move(actions.DiscreteActions, transform);
+        
+        if (transform.localPosition.y < -1f)
+        {
+            SetReward(-1f);
+            EndEpisode();
+        }
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        var discreteActionsOut = actionsOut.DiscreteActions;
+        discreteActionsOut.Clear();
+        if (Input.GetKey(KeyCode.W))
+        {
+            discreteActionsOut[0] = 1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            discreteActionsOut[1] = 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            discreteActionsOut[0] = 2;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            discreteActionsOut[1] = 2;
+        }
+        discreteActionsOut[2] = Input.GetKey(KeyCode.Space) ? 1 : 0;
+    }
+
+    public void OnCollisionEnter(Collision col)
+    {
+        // var colGameObject = col.gameObject;
+        // var clearTarget = colGameObject.GetComponent<IClearEvent>();
+        // var badTarget = colGameObject.GetComponent<IBadEvent>();
+        // if (badTarget != null)
+        // {
+        //     badTarget.BadEvent();
+        //     StateProcessor.State.Value = StateDie;
+        // }
+        // else if (clearTarget != null)
+        // {
+        //     clearTarget.ClearEvent();
+        //     StateProcessor.State.Value = StateClear;
+        // }
+        if (col.gameObject.CompareTag("Goal"))
+        {
+            SetReward(1f);
+            EndEpisode();
+        }
+    }
+
+    void ResetGoal()
+    {
+        var randomPosNum = Random.Range(0, goalRespawnPos.Length - 1);
+        
+        m_TargetRb.transform.position = goalRespawnPos[randomPosNum].transform.position;
     }
 
     protected  virtual void FixedUpdate()
     {
         var localTransform = transform;
         isGroundRay = GenerateRay(localTransform);
-        Debug.DrawRay(isGroundRay.origin, isGroundRay.direction * _distance, Color.red);
         isGround = GroundCheck(isGroundRay);
         _animator.SetBool("is_Ground", isGround);
-        Move(localTransform);
     }
 
-    protected void OnCollisionEnter(Collision col)
-    {
-        var colGameObject = col.gameObject;
-        var clearTarget = colGameObject.GetComponent<IClearEvent>();
-        var badTarget = colGameObject.GetComponent<IBadEvent>();
-        if (badTarget != null)
-        {
-            badTarget.BadEvent();
-            StateProcessor.State.Value = StateDie;
-        }
-        else if (clearTarget != null)
-        {
-            clearTarget.ClearEvent();
-            StateProcessor.State.Value = StateClear;
-        }
-    }
+    
 
-    protected Ray GenerateRay(Transform nowTransform)
+    Ray GenerateRay(Transform nowTransform)
     {
         var rayPosition = nowTransform.position + new Vector3(0, 0.5f, 0);
          var isGroundRay = new Ray(rayPosition, nowTransform.up * -1);
@@ -127,28 +193,42 @@ public class EnemyAgent : Agent
     }
     
 
-    protected bool GroundCheck(Ray isGroundRay)
+    bool GroundCheck(Ray isGroundRay)
     {
         if (!Physics.Raycast(isGroundRay, out _isGroundHit, _distance)) return false;
         if (!_isGroundHit.collider.CompareTag("Ground")) return false;
         return true;
     }
 
-    protected void Move(Transform localTransform)
+    public void Move(ActionSegment<int> act, Transform localTransform)
     {
         AddReward(-1 / MaxStep);
+
+        var dirToGoForwardAction = act[0];
+        var dirToGoSideAction = act[1];
+        var jumpAction = act[2];
+        var dirToGo_X = 0;
+        var dirToGo_Z = 0;
         
         var localTransformPosition = localTransform.position;
         var nowSpeed_X = math.abs((localTransformPosition.x - latestPos.x) / Time.deltaTime);
         var nowSpeed_Z = math.abs((localTransformPosition.z - latestPos.z) / Time.deltaTime);
-        latestPos = localTransformPosition; 
-        
-        Vector3 moveVector3 = new Vector3(_playerInput.X, 0, _playerInput.Z);
+        latestPos = localTransformPosition;
+
+        if (dirToGoForwardAction == 1)
+            dirToGo_Z = -1;
+        else if (dirToGoForwardAction == 2)
+            dirToGo_Z= 1;
+        if (dirToGoSideAction == 1)
+            dirToGo_X = -1;
+        else if (dirToGoSideAction == 2)
+            dirToGo_X = 1;
+        Vector3 moveVector3 = new Vector3(dirToGo_X, 0, dirToGo_Z);
         if (moveVector3.magnitude > 1)
         {
             moveVector3.Normalize();
         }
-        m_Agentrb.AddForce(moveVector3 * RUN_SPEED);
+        m_AgentRb.AddForce(moveVector3 * RUN_SPEED);
 
         _animator.SetFloat("Horizontal_Vel",math.abs(moveVector3.x));
         _animator.SetFloat("Vertical_Vel",math.abs(moveVector3.z));
@@ -157,10 +237,11 @@ public class EnemyAgent : Agent
         
         if (isGround)
         {
-            if (_playerInput.Jump == true)
+            if (jumpAction == 1)
             {
                 _animator.SetTrigger("is_Jump");
-                m_Agentrb.velocity = Vector3.up * JUMP_POWER;
+                m_AgentRb.velocity = Vector3.up * JUMP_POWER;
+                AddReward(-0.01f);
             }
             else if (nowSpeed_X > 0.1 || nowSpeed_Z > 0.1)
             {
@@ -175,7 +256,7 @@ public class EnemyAgent : Agent
             {
                 StateProcessor.State.Value = StateIdle;
                 _animator.SetTrigger("is_Idle");
-                m_Agentrb.angularVelocity = new Vector3(0f, 0f, 0f);
+                m_AgentRb.angularVelocity = new Vector3(0f, 0f, 0f);
             }
         }
         else
@@ -191,10 +272,10 @@ public class EnemyAgent : Agent
             }
         }
 
-        if (localTransform.position.y < -10)
-        {
-            StateProcessor.State.Value = StateDie;
-        }
+        // if (localTransform.position.y < -10)
+        // {
+        //     StateProcessor.State.Value = StateDie;
+        // }
     }
     
     public void Idle()
@@ -224,6 +305,6 @@ public class EnemyAgent : Agent
     public void Die()
     {
         _animator.SetBool("is_Run", false);
-        Destroy(gameObject);
+        // Destroy(gameObject);
     }
 }
